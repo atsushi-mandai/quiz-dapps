66773343aa1fc967bdf8769c08943fa1827cdd9b
"atsushi-mandai" <"atsushi.mandai.1991@gmail.com">
2022-06-07 20:51:31 +0900

    imported several basic files from OpenZeppelin

---
+++ contracts/CreateQuiz.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.8.13;
+
+import "./Ownable.sol";
+
+contract CreateQuiz is Ownable {
+    //I will write things here.
+}
--- contracts/test.sol
+++
@@ -1,2 +0,0 @@
-//this is a teset
-// i did pull, now i do push
