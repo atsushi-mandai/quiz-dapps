aa414b5775c4acd5fd38de743ddf10ac7ff1c51d
"atsushi-mandai" <"atsushi.mandai.1991@gmail.com">
2022-06-08 09:41:06 +0900

    fixed spelling mistake of mapping "quizzIsActive" to "quizIsActive"

--- contracts/CreateQuiz.sol
+++ contracts/CreateQuiz.sol
@@ -13,25 +13,28 @@ contract CreateQuiz is Ownable {
         string choice2;
         string choice3;
         string choice4;
+        string hint;
         uint8 answer;
         uint8 languageCode;
     }
 
     Quiz[] internal quizzes;
 
-    mapping (uint => bool) public quizzIsActive;
+    mapping (uint => bool) public quizIsActive;
+    mapping (uint => address) public quizOwner;
 
     //@dev Create a Quiz. Will call this function from other functions.
-    function _createQuiz(string memory _question, string memory _choice1, string memory _choice2, string memory _choice3, string memory _choice4, uint8 _answer, uint8 _languageCode) private {
-        quizzes.push(Quiz(_question, _choice1, _choice2, _choice3, _choice4, _answer, _languageCode));
+    function _createQuiz(string memory _question, string memory _choice1, string memory _choice2, string memory _choice3, string memory _choice4, string _hint, uint8 _answer, uint8 _languageCode) private {
+        quizzes.push(Quiz(_question, _choice1, _choice2, _choice3, _choice4, _hint, _answer, _languageCode));
         uint id = quizzes.length - 1;
-        quizzIsActive[id] = true;
+        quizOwner[id] = msg.sender;
+        quizIsActive[id] = true;
         emit NewQuiz(_question, _answer);
     }
 
     //@dev Giving contract owner a power to change the status of each quiz for now.
     function changeQuizStatus(uint _quizId ,bool _quizStatus) public onlyOwner {
-        quizzIsActive[_quizId] = _quizStatus;
+        quizIsActive[_quizId] = _quizStatus;
     }
 
 }
