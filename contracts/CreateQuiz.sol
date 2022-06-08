17987a592a1aa38c102dae2d31af82524850b717
"atsushi-mandai" <"atsushi.mandai.1991@gmail.com">
2022-06-08 10:14:42 +0900

    added function to let quiz owner change the status

--- contracts/CreateQuiz.sol
+++ contracts/CreateQuiz.sol
@@ -23,8 +23,13 @@ contract CreateQuiz is Ownable {
     mapping (uint => bool) public quizIsActive;
     mapping (uint => address) public quizOwner;
 
+    modifier onlyQuizOwner(uint _quizId) { 
+        require(quizOwner[_quizId] == msg.sender);
+        _;
+    }
+
     //@dev Create a Quiz. Will call this function from other functions.
-    function _createQuiz(string memory _question, string memory _choice1, string memory _choice2, string memory _choice3, string memory _choice4, string _hint, uint8 _answer, uint8 _languageCode) private {
+    function _createQuiz(string memory _question, string memory _choice1, string memory _choice2, string memory _choice3, string memory _choice4, string memory _hint, uint8 _answer, uint8 _languageCode) private {
         quizzes.push(Quiz(_question, _choice1, _choice2, _choice3, _choice4, _hint, _answer, _languageCode));
         uint id = quizzes.length - 1;
         quizOwner[id] = msg.sender;
@@ -32,8 +37,13 @@ contract CreateQuiz is Ownable {
         emit NewQuiz(_question, _answer);
     }
 
+    //@dev Let quiz owner change the status of each quiz
+    function quizOwnerChangeStatus(uint _quizId, bool _quizStatus) public onlyQuizOwner(_quizId) {
+        quizIsActive[_quizId] = _quizStatus;
+    }
+
     //@dev Giving contract owner a power to change the status of each quiz for now.
-    function changeQuizStatus(uint _quizId ,bool _quizStatus) public onlyOwner {
+    function changeQuizStatus(uint _quizId, bool _quizStatus) public onlyOwner {
         quizIsActive[_quizId] = _quizStatus;
     }
 
