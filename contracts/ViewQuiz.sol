581b5489ed13f140d0f613200c1f3468ecf964b3
"atsushi-mandai" <"atsushi.mandai.1991@gmail.com">
2022-06-08 10:37:31 +0900

    added getQuiz function

---
+++ contracts/ViewQuiz.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "./CreateQuiz.sol";
+
+contract ViewQuiz is CreateQuiz {
+
+    struct QuizQuestion {
+        string question;
+        string choice1;
+        string choice2;
+        string choice3;
+        string choice4;
+        string hint;
+    }
+
+    function getQuiz(uint _quizId) public view returns (QuizQuestion memory) {
+        Quiz memory quiz = quizzes[_quizId];
+        return QuizQuestion(quiz.question, quiz.choice1, quiz.choice2, quiz.choice3, quiz.choice4, quiz.hint);
+    }
+
+    
+
+}
