cbb3f669289466b78726e21e649fbc7a1d2a9dfa
"atsushi-mandai" <"atsushi.mandai.1991@gmail.com">
2022-06-10 14:05:49 +0900

    added reward and rewardpool to Quiz, _createQuiz

--- contracts/CreateQuiz.sol
+++ contracts/CreateQuiz.sol
@@ -15,6 +15,8 @@ contract CreateQuiz is Ownable {
         string choice4;
         string hint;
         uint8 languageCode;
+        uint reward;
+        uint rewardPool;
     }
 
     Quiz[] internal quizzes;
@@ -29,8 +31,8 @@ contract CreateQuiz is Ownable {
     }
 
     //@dev Create a Quiz. Will call this function from other functions.
-    function _createQuiz(string memory _question, string memory _choice1, string memory _choice2, string memory _choice3, string memory _choice4, string memory _hint, uint8 _answer, uint8 _languageCode) private {
-        quizzes.push(Quiz(_question, _choice1, _choice2, _choice3, _choice4, _hint, _languageCode));
+    function _createQuiz(string memory _question, string memory _choice1, string memory _choice2, string memory _choice3, string memory _choice4, string memory _hint, uint8 _answer, uint8 _languageCode, uint _reward, uint _rewardPool) private {
+        quizzes.push(Quiz(_question, _choice1, _choice2, _choice3, _choice4, _hint, _languageCode, _reward, _rewardPool));
         uint id = quizzes.length - 1;
         quizAnswer[id] = _answer;
         quizOwner[id] = msg.sender;
